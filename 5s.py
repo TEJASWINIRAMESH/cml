1fe7958528c4d026af93a4a639e4fbb6b0a6f76b
TEJASWINIRAMESH <115721265+TEJASWINIRAMESH@users.noreply.github.com>
2024-11-15 06:43:54 +0530

    Update 5s.py

--- 5s.py
+++ 5s.py
@@ -22,3 +22,27 @@ print("File received and saved as 'received_file.txt'.")
 # Close connections
 client_socket.close()
 server_socket.close()
+
+# updated code 
+import socket
+
+def Server():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(("localhost",5000))
+    server.listen(1)
+
+    print("Sever is listening...")
+
+    client, add = server.accept()
+
+    print(f"Connection established at {add}")
+
+    data = client.recv(1024).decode('utf-8')
+
+    with open("Received.txt", "w") as server_file:
+        server_file.write(data)
+
+    client.sendall("File saved".encode('utf-8'))
+
+Server()
+
